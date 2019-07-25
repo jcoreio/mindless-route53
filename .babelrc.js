720b665eee4e45425a0b79f0c930f23641e558b5
Andy Edwards <jedwards@fastmail.com>
2019-07-25 12:03:21 -0500

    build: strip flow types before class properties transform

--- .babelrc.js
+++ .babelrc.js
@@ -4,11 +4,11 @@ module.exports = function(api) {
       'babel-plugin-flow-runtime',
       { assert: false, annotate: false, optInOnly: true },
     ],
+    '@babel/plugin-transform-flow-strip-types',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-flow-strip-types',
   ]
   const presets = [
     [
