a26978fdb55be5dfd5f52fa66375558a55fd8d99
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2019-09-10 10:31:18 -0500

    refactor: share hosted zones and Route53 simulator between all tests

--- test/index.js
+++ test/index.js
@@ -4,82 +4,89 @@ import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import { findHostedZoneId, upsertRecordSet } from '../src'
 
+const HOSTED_ZONES = [
+  {
+    Id: '/hostedzone/AAAAAAAAAAAAA',
+    Name: 'bar.io.',
+    Config: { PrivateZone: false },
+    ResourceRecordSetCount: 67,
+  },
+  {
+    Id: '/hostedzone/BBBBBBBBBBBBB',
+    Name: 'foo.io.',
+    Config: { PrivateZone: true },
+    ResourceRecordSetCount: 68,
+  },
+  {
+    Id: '/hostedzone/AAAAAAAAAAAAA',
+    Name: 'jcore.io.',
+    Config: { PrivateZone: false },
+    ResourceRecordSetCount: 67,
+  },
+  {
+    Id: '/hostedzone/BBBBBBBBBBBBB',
+    Name: 'jcore.io.',
+    Config: { PrivateZone: true },
+    ResourceRecordSetCount: 68,
+  },
+  {
+    Id: '/hostedzone/CCCCCCCCCCCCC',
+    Name: 'foo.jcore.io.',
+    Config: { PrivateZone: false },
+    ResourceRecordSetCount: 67,
+  },
+  {
+    Id: '/hostedzone/DDDDDDDDDDDDD',
+    Name: 'foo.jcore.io.',
+    Config: { PrivateZone: true },
+    ResourceRecordSetCount: 68,
+  },
+  {
+    Id: '/hostedzone/EEEEEEEEEEEEE',
+    Name: 'bar.jcore.io.',
+    Config: { PrivateZone: false },
+    ResourceRecordSetCount: 67,
+  },
+  {
+    Id: '/hostedzone/FFFFFFFFFFFFF',
+    Name: 'bar.jcore.io.',
+    Config: { PrivateZone: true },
+    ResourceRecordSetCount: 68,
+  },
+]
+
+const changeResourceRecordSetsArgs = []
+
+const Route53 = {
+  listHostedZonesByName: ({ DNSName, HostedZoneId }) => ({
+    promise: async () => {
+      const i = HOSTED_ZONES.findIndex(
+        z =>
+          z.Id === HostedZoneId || z.Name.endsWith(DNSName.replace(/\.?$/, '.'))
+      )
+      return i < 0
+        ? { HostedZones: [], IsTruncated: false }
+        : {
+            HostedZones: HOSTED_ZONES.slice(i, i + 2),
+            IsTruncated: i + 2 < HOSTED_ZONES.length,
+            NextHostedZoneId:
+              i + 2 < HOSTED_ZONES.length ? HOSTED_ZONES[i + 2].Id : null,
+            NextDNSName:
+              i + 2 < HOSTED_ZONES.length ? HOSTED_ZONES[i + 2].Name : null,
+          }
+    },
+  }),
+  changeResourceRecordSets: arg => ({
+    promise: async () => {
+      changeResourceRecordSetsArgs.push(arg)
+      return { ChangeInfo: { Id: 'xxxxxx' } }
+    },
+  }),
+  waitFor: () => ({ promise: async () => {} }),
+}
+
 describe(`findHostedZoneId`, function() {
   it(`works`, async function(): Promise<void> {
-    const zones = [
-      {
-        Id: '/hostedzone/AAAAAAAAAAAAA',
-        Name: 'bar.io.',
-        Config: { PrivateZone: false },
-        ResourceRecordSetCount: 67,
-      },
-      {
-        Id: '/hostedzone/BBBBBBBBBBBBB',
-        Name: 'foo.io.',
-        Config: { PrivateZone: true },
-        ResourceRecordSetCount: 68,
-      },
-      {
-        Id: '/hostedzone/AAAAAAAAAAAAA',
-        Name: 'jcore.io.',
-        Config: { PrivateZone: false },
-        ResourceRecordSetCount: 67,
-      },
-      {
-        Id: '/hostedzone/BBBBBBBBBBBBB',
-        Name: 'jcore.io.',
-        Config: { PrivateZone: true },
-        ResourceRecordSetCount: 68,
-      },
-      {
-        Id: '/hostedzone/CCCCCCCCCCCCC',
-        Name: 'foo.jcore.io.',
-        Config: { PrivateZone: false },
-        ResourceRecordSetCount: 67,
-      },
-      {
-        Id: '/hostedzone/DDDDDDDDDDDDD',
-        Name: 'foo.jcore.io.',
-        Config: { PrivateZone: true },
-        ResourceRecordSetCount: 68,
-      },
-      {
-        Id: '/hostedzone/EEEEEEEEEEEEE',
-        Name: 'bar.jcore.io.',
-        Config: { PrivateZone: false },
-        ResourceRecordSetCount: 67,
-      },
-      {
-        Id: '/hostedzone/FFFFFFFFFFFFF',
-        Name: 'bar.jcore.io.',
-        Config: { PrivateZone: true },
-        ResourceRecordSetCount: 68,
-      },
-    ]
-
-    const Route53 = {
-      listHostedZonesByName: ({ DNSName, HostedZoneId }) => ({
-        promise: () => {
-          const i = zones.findIndex(
-            z =>
-              z.Id === HostedZoneId ||
-              z.Name.endsWith(DNSName.replace(/\.?$/, '.'))
-          )
-          return Promise.resolve(
-            i < 0
-              ? { HostedZones: [], IsTruncated: false }
-              : {
-                  HostedZones: zones.slice(i, i + 2),
-                  IsTruncated: i + 2 < zones.length,
-                  NextHostedZoneId:
-                    i + 2 < zones.length ? zones[i + 2].Id : null,
-                  NextDNSName: i + 2 < zones.length ? zones[i + 2].Name : null,
-                }
-          )
-        },
-      }),
-    }
-
     expect(
       await findHostedZoneId({
         DNSName: 'glob.foo.jcore.io',
@@ -120,86 +127,6 @@ describe(`findHostedZoneId`, function() {
 })
 
 describe(`upsertRecordSet`, function() {
-  const zones = [
-    {
-      Id: '/hostedzone/AAAAAAAAAAAAA',
-      Name: 'bar.io.',
-      Config: { PrivateZone: false },
-      ResourceRecordSetCount: 67,
-    },
-    {
-      Id: '/hostedzone/BBBBBBBBBBBBB',
-      Name: 'foo.io.',
-      Config: { PrivateZone: true },
-      ResourceRecordSetCount: 68,
-    },
-    {
-      Id: '/hostedzone/AAAAAAAAAAAAA',
-      Name: 'jcore.io.',
-      Config: { PrivateZone: false },
-      ResourceRecordSetCount: 67,
-    },
-    {
-      Id: '/hostedzone/BBBBBBBBBBBBB',
-      Name: 'jcore.io.',
-      Config: { PrivateZone: true },
-      ResourceRecordSetCount: 68,
-    },
-    {
-      Id: '/hostedzone/CCCCCCCCCCCCC',
-      Name: 'foo.jcore.io.',
-      Config: { PrivateZone: false },
-      ResourceRecordSetCount: 67,
-    },
-    {
-      Id: '/hostedzone/DDDDDDDDDDDDD',
-      Name: 'foo.jcore.io.',
-      Config: { PrivateZone: true },
-      ResourceRecordSetCount: 68,
-    },
-    {
-      Id: '/hostedzone/EEEEEEEEEEEEE',
-      Name: 'bar.jcore.io.',
-      Config: { PrivateZone: false },
-      ResourceRecordSetCount: 67,
-    },
-    {
-      Id: '/hostedzone/FFFFFFFFFFFFF',
-      Name: 'bar.jcore.io.',
-      Config: { PrivateZone: true },
-      ResourceRecordSetCount: 68,
-    },
-  ]
-
-  const changeResourceRecordSetsArgs = []
-
-  const Route53 = {
-    listHostedZonesByName: ({ DNSName, HostedZoneId }) => ({
-      promise: async () => {
-        const i = zones.findIndex(
-          z =>
-            z.Id === HostedZoneId ||
-            z.Name.endsWith(DNSName.replace(/\.?$/, '.'))
-        )
-        return i < 0
-          ? { HostedZones: [], IsTruncated: false }
-          : {
-              HostedZones: zones.slice(i, i + 2),
-              IsTruncated: i + 2 < zones.length,
-              NextHostedZoneId: i + 2 < zones.length ? zones[i + 2].Id : null,
-              NextDNSName: i + 2 < zones.length ? zones[i + 2].Name : null,
-            }
-      },
-    }),
-    changeResourceRecordSets: arg => ({
-      promise: async () => {
-        changeResourceRecordSetsArgs.push(arg)
-        return { ChangeInfo: { Id: 'xxxxxx' } }
-      },
-    }),
-    waitFor: () => ({ promise: async () => {} }),
-  }
-
   beforeEach(() => {
     changeResourceRecordSetsArgs.length = 0
   })
