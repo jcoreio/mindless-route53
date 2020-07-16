9ca9060a45d154550a69c68616c67e2fb4bc1059
Andy Edwards <jedwards@fastmail.com>
2020-07-15 22:34:25 -0500

    fix: add TypeScript defs for upsertPublicAndPrivateRecords

--- index.d.ts
+++ index.d.ts
@@ -46,3 +46,20 @@ export type UpsertRecordSetOptions = {
 }
 
 export function upsertRecordSet(options: UpsertRecordSetOptions): Promise<void>
+
+export type UpsertPublicAndPrivateRecordsOptions = {
+  Name: string
+  TTL?: number
+  PrivateTarget: string | Array<string>
+  PublicTarget: string | Array<string>
+  PublicHostedZone?: AWS.Route53.HostedZone | null | undefined
+  PrivateHostedZone?: AWS.Route53.HostedZone | null | undefined
+  Route53?: AWS.Route53 | null | undefined
+  waitForChanges?: boolean | null | undefined
+  log?: ((...args: Array<any>) => any) | null | undefined
+  verbose?: boolean | null | undefined
+}
+
+export function upsertPublicAndPrivateRecords(
+  options: UpsertPublicAndPrivateRecordsOptions
+): Promise<void>
