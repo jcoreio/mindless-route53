5fe0f6a6df89aa719a56900cdb5158a0795e5ad5
Andy Edwards <jedwards@fastmail.com>
2026-02-25 14:40:36 -0600

    fix: target node 16 in CJS

--- toolchain.config.cjs
+++ toolchain.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  cjsBabelEnv: { forceAllTransforms: true },
+  cjsBabelEnv: { targets: { node: 16 } },
   esmBabelEnv: { targets: { node: 16 } },
   // outputEsm: false, // disables ESM output (default: true)
   // esWrapper: true, // outputs ES module wrappers for CJS modules (default: false)
